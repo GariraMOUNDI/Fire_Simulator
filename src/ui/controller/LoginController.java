95c3abd3578835f6919dbd6ee29dd2e18cc2c522
mazou <mazougarira@yahoo.fr>
2019-12-12 09:46:55 +0100

    Exemple Java FX

--- src/ui/controller/LoginController.java
+++ src/ui/controller/LoginController.java
@@ -1,5 +1,28 @@
 package ui.controller;
 
+import com.sun.prism.paint.Color;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Background;
+import ui.model.LoginUI;
+
 public class LoginController {
 
+    @FXML
+    Button bonjour;
+    @FXML
+    TextArea text;
+
+
+    public void clikedButton(ActionEvent actionEvent) {
+        System.out.println("Button clicked");
+        text.setText(text.getText() + "\nParfait");
+    }
+
+    public static void main(String[] args) {
+        Application.launch(LoginUI.class);
+    }
 }
--- src/ui/model/LoginUI.java
+++ src/ui/model/LoginUI.java
@@ -1,5 +1,19 @@
 package ui.model;
 
-public class LoginUI {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class LoginUI extends Application {
+
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../views/LoginView.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
 
 }
