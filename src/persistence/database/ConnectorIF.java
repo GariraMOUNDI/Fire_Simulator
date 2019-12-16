25a6fa276eb74feecfe00df131fdf2557707e14d
mazou <mazougarira@yahoo.fr>
2019-12-16 12:49:15 +0100

    Connector Interface OK

--- src/persistence/database/ConnectorIF.java
+++ src/persistence/database/ConnectorIF.java
@@ -1,4 +1,11 @@
 package persistence.database;
 
 public interface ConnectorIF {
+    public void connectionToDatabase();
+    public Object getData(Object arg);
+    public void updateData(Object arg);
+    public void deleteData(Object arg);
+    public void insertData(Object arg);
+    public void closeConnection();
+    public void connectionException(Object arg);
 }
