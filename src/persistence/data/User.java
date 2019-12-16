81748e6ca9c490135cb4063bc1b39ac18b134896
VincentVBB <vincent.v2b@gmail.com>
2019-12-16 11:16:14 +0100

    Implementation of SessionFacade, UserDAO, and User classes

--- src/persistence/data/User.java
+++ src/persistence/data/User.java
@@ -1,4 +1,13 @@
 package persistence.data;
 
 public class User {
+    private String username, password;
+
+    public User(String username, String password) {
+
+    }
+
+    public String getPassword(){
+        return password;
+    }
 }
