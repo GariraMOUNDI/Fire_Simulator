9c4dce27e36972cb98521701d0cfd533a6afc25a
VincentVBB <vincent.v2b@gmail.com>
2019-12-16 11:16:14 +0100

    Implementation of SessionFacade, UserDAO, and User classes

--- src/businessLogic/SessionFacade.java
+++ src/businessLogic/SessionFacade.java
@@ -1,20 +1,39 @@
 package businessLogic;
 
 import persistence.AbstractFactory;
+import persistence.DAOFactory;
 import persistence.dao.UserDAO;
+import persistence.data.User;
 import ui.interfaces.LoginInterface;
+
 public class SessionFacade {
     private LoginInterface loginIF;
     private UserDAO dao;
+    private DAOFactory factory;
+    private User userLoggedIn;
 
     public SessionFacade(LoginInterface loginIF) {
         this.loginIF = loginIF;
-
     }
 
     public boolean login(String username, String password){
+        User tempUser = dao.getByUsername(username);
+        return checkCredentials(tempUser, password);
+    }
 
+    private boolean checkCredentials(User user, String password) {
+        if (user.getPassword().equals(password)){
+            setUserLoggedIn(user);
+            return true;
+        }
+        return false;
+    }
 
-        return true;
+    private void setUserLoggedIn(User user) {
+        this.userLoggedIn = user;
+    }
+
+    public User getUser(){
+        return userLoggedIn;
     }
 }
