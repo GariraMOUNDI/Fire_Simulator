5bdb30c105f3e32b3dcd6aaeade27e60e6127e64
mazou <mazougarira@yahoo.fr>
2019-12-17 23:14:20 +0100

    Login Designed (Version 1)

--- src/persistence/factories/DAOFactory.java
+++ src/persistence/factories/DAOFactory.java
@@ -1,9 +1,9 @@
-package persistence;
+package persistence.factories;
 
 public class DAOFactory extends AbstractFactory {
     // Singleton Pattern
     private static DAOFactory dao = null;
-    public static DAOFactory getFactory(DAOType type){
+    public static DAOFactory getFactory(){
         if (dao == null)
             dao = new DAOFactory();
         return dao;
