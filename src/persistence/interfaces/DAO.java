52736b8163b87e6d00a1d3fec6ceef50d1ce7877
mazou <mazougarira@yahoo.fr>
2019-12-19 15:35:50 +0100

    Login Designed (Version 3)

--- src/persistence/interfaces/DAO.java
+++ src/persistence/interfaces/DAO.java
@@ -1,13 +1,13 @@
 package persistence.interfaces;
 
-import persistence.factories.DAOType;
-
-import java.util.Optional;
-
 public interface DAO<T> {
-    public Optional<T> get(T obj);
-    public void save(T obj);
-    public void update(T obj);
-    public Object getAll();
-    public void delete(T obj);
+    public void connectionToDatabase();
+    public Object getAllData();
+    public Object getDataById(String key, Object value);
+    public void updateData(T arg);
+    public void deleteData(Object arg);
+    public void insertData(T arg);
+    public void closeConnection();
+    public void connectionException(Object arg);
+    public void setCollectionName(String arg);
 }
