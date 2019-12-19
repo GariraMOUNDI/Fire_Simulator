cf5fd206b48188b1aacb4e27d9b87decd2dd18df
mazou <mazougarira@yahoo.fr>
2019-12-19 15:35:50 +0100

    Login Designed (Version 3)

--- src/persistence/data/User.java
+++ src/persistence/data/User.java
@@ -5,19 +5,17 @@ import org.bson.Document;
 import java.io.Serializable;
 
 public class User {
-    private String username, password, email;
+    private String username, password;
 
-    public User(String username, String password, String email) {
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.email = email;
     }
 
     public String getPassword(){
         return password;
     }
     public String getUsername() { return username; }
-    public String getEmail() { return email; }
     public String toString(){
         return username + " : " + password ;
     }
