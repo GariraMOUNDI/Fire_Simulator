78606f9c27d6d11eb8ccaaf670c39130caec60af
VincentVBB <vincent.v2b@gmail.com>
2019-12-21 14:13:29 +0100

    change on LoginController, modelUI, and views to switch windows.

--- src/ui/model/LoginUI.java
+++ src/ui/model/LoginUI.java
@@ -6,14 +6,36 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 
 public class LoginUI extends Application {
 
+    private static Stage primaryStage;
+
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../views/LoginView.fxml"));
-        primaryStage.setTitle("Hello World");
+        loginView(primaryStage);
+    }
+
+    public static void loginView(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(LoginUI.class.getResource("../views/LoginView.fxml"));
+        primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        LoginUI.primaryStage = primaryStage;
+    }
+
+
+
+    public static void registerView(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(LoginUI.class.getResource("../views/RegisterView.fxml"));
+        stage.setTitle("Register");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static Stage getStage(){
+        return primaryStage;
     }
 
 }
