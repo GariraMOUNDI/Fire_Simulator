077c0bed7a81df76f903634d32b9d11cf7b17233
Remy McConnell <remy.m@floversal.com>
2020-01-11 16:12:41 +0100

    javadocs register controller

--- src/ui/interfaces/LoginInterface.java
+++ src/ui/interfaces/LoginInterface.java
@@ -1,5 +1,13 @@
 package ui.interfaces;
 
+/**
+ * The interface Login interface. This interface is implemented by all of the controllers
+ */
 public interface LoginInterface {
+    /**
+     * Print results.
+     *
+     * @param arg the arg
+     */
     public void printResults(Object arg);
 }
