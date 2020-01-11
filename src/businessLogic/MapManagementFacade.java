57f3ccccaa74b256a370412450796a21b9f9d76d
Remy McConnell <remy.m@floversal.com>
2020-01-11 17:52:49 +0100

    javadocs facades

--- src/businessLogic/MapManagementFacade.java
+++ src/businessLogic/MapManagementFacade.java
@@ -11,6 +11,9 @@ import ui.interfaces.LoginInterface;
 
 import java.util.List;
 
+/**
+ * The type Map management facade.
+ */
 public class MapManagementFacade {
     private LoginInterface loginIF;
     private DAO dao;
@@ -18,10 +21,20 @@ public class MapManagementFacade {
     private static MapManagementFacade instance = null;
 
 
+    /**
+     * Gets current terrain.
+     *
+     * @return the current terrain
+     */
     public Terrain getCurrentTerrain() {
         return currentTerrain;
     }
 
+    /**
+     * Sets current terrain.
+     *
+     * @param currentTerrain the current terrain
+     */
     public void setCurrentTerrain(Terrain currentTerrain) {
         this.currentTerrain = currentTerrain;
     }
@@ -29,23 +42,54 @@ public class MapManagementFacade {
         this.loginIF = loginIF;
         dao = MongoDBDAOFactory.getInstance().createDAO(DAOType.Terrain);
     }
+
+    /**
+     * Get the instance of map management facade.
+     *
+     * @param loginIF the login interface
+     * @return the map management facade
+     */
     public static MapManagementFacade getInstance(LoginInterface loginIF){
         if (instance == null)
             instance = new MapManagementFacade(loginIF);
         return instance;
     }
 
+    /**
+     * Get map terrain.
+     *
+     * @param _id the id
+     * @return the terrain
+     */
     public Terrain getMap(Object _id){return (Terrain) dao.getDataById("_id", _id);}
 
+    /**
+     * Get user maps list.
+     *
+     * @param username the username
+     * @return the list
+     */
     public List<Terrain> getUserMaps(String username){
         return (List<Terrain>) dao.getDataById("username", username);
     }
 
+    /**
+     * Delete map.
+     *
+     * @param map the map
+     */
     public void deleteMap(Terrain map){
         dao.deleteData(map);
     }
 
 
+    /**
+     * Create map.
+     *
+     * @param name     the name
+     * @param map      the map
+     * @param username the username
+     */
     public void createMap(String name, Matrice map, String username) {
         // TODO
         // BE SURE NAMES ARE UNIQUE
@@ -54,6 +98,11 @@ public class MapManagementFacade {
 
     }
 
+    /**
+     * Save map.
+     *
+     * @param map the map
+     */
     public void saveMap(Terrain map){
         dao.updateData(map);
     }
--- src/businessLogic/SessionFacade.java
+++ src/businessLogic/SessionFacade.java
@@ -11,6 +11,9 @@ import ui.interfaces.LoginInterface;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The type Session facade.
+ */
 public class SessionFacade {
     private LoginInterface loginIF;
     private DAO dao;
@@ -20,6 +23,12 @@ public class SessionFacade {
     private static SessionFacade instance = null;
     private int characterIndex;
 
+    /**
+     * Get instance session facade.
+     *
+     * @param loginIF the login if
+     * @return the session facade
+     */
     public static SessionFacade getInstance(LoginInterface loginIF){
         if (instance == null)
             instance = new SessionFacade(loginIF);
@@ -31,18 +40,43 @@ public class SessionFacade {
         dao = MongoDBDAOFactory.getInstance().createDAO(DAOType.User);
     }
 
+    /**
+     * Exists. Checks if the user already exists in the database
+     *
+     * @param type       the type
+     * @param credential the credential
+     * @return the boolean
+     */
     public boolean exists(String type, String credential) {
         return dao.getDataById(type, credential) instanceof User;
     }
 
+    /**
+     * Login.
+     *
+     * @param username the username
+     * @param password the password
+     * @return true or false depending on the success of the login
+     */
     public boolean login(String username, String password){
         return checkCredentials((User) dao.getDataById("username", username), password);
     }
 
+    /**
+     * Logout.
+     */
     public void logout() {
         userLoggedIn = null;
     }
 
+    /**
+     * Register.
+     *
+     * @param username the username
+     * @param password the password
+     * @param email    the email
+     * @param helpWord the help word
+     */
     public void register(String username, String password, String email, String helpWord) {
 
         dao.insertData(new User(username, password, email, helpWord));
@@ -61,31 +95,64 @@ public class SessionFacade {
         }
     }
 
+    /**
+     * Sets user logged in.
+     *
+     * @param user the user
+     */
     public void setUserLoggedIn(User user) {
         this.userLoggedIn = user;
     }
 
+    /**
+     * Get user logged in user.
+     *
+     * @return the user
+     */
     public User getUserLoggedIn(){
         return userLoggedIn;
     }
 
-    // Methods for the management of friends
+    /**
+     * Update user.
+     */
+// Methods for the management of friends
     public void updateUser(){
         dao.updateData(userLoggedIn);
     }
 
+    /**
+     * Get user logged in friends list.
+     *
+     * @return the list
+     */
     public List<String> getUserLoggedInFriends(){
         return userLoggedIn.getFriends();
     }
 
+    /**
+     * Add friend to user logged in.
+     *
+     * @param arg the arg
+     */
     public void addFriendToUserLoggedIn(String arg){
         userLoggedIn.addFriend(arg);
     }
 
+    /**
+     * Remove friend to user logged in.
+     *
+     * @param arg the arg
+     */
     public void removeFriendToUserLoggedIn(String arg){
         userLoggedIn.removeFriend(arg);
     }
 
+    /**
+     * Get other user list.
+     *
+     * @return the list
+     */
     public List<String> getOtherUser(){
         others = new ArrayList<>();
         friends = getUserLoggedInFriends();
@@ -96,10 +163,21 @@ public class SessionFacade {
         return others;
     }
 
+    /**
+     * Get user logged in items list.
+     *
+     * @return the list
+     */
     public List<Item> getUserLoggedInItems(){
         return userLoggedIn.getItems();
     }
 
+    /**
+     * Get character at specified index.
+     *
+     * @param characterIndex the character index
+     * @return the located character, or null
+     */
     public Character getCharacterAt(int characterIndex){
         try{
             return getCharacters().get(characterIndex);
@@ -108,20 +186,38 @@ public class SessionFacade {
         }
     }
 
+    /**
+     * Get characters list.
+     *
+     * @return the list of characters
+     */
     public List<Character> getCharacters(){
         return userLoggedIn.getCharacters();
     }
 
+    /**
+     * Get next character.
+     *
+     * @return the character
+     */
     public Character nextCharacter() {
         characterIndex = (characterIndex + 1) % getCharacters().size();
         return getCharacterAt(characterIndex);
     }
 
+    /**
+     * Get previous character.
+     *
+     * @return the character
+     */
     public Character previousCharacter() {
         characterIndex = (characterIndex - 1 + getCharacters().size()) % (getCharacters().size());
         return getCharacterAt(characterIndex);
     }
 
+    /**
+     * Delete account.
+     */
     public void deleteAccount() {
         dao.deleteData(userLoggedIn);
         userLoggedIn = null;
