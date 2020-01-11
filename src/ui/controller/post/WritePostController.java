eef7d09c3b00ca1d96c4315f4698be7dadd91ee2
Remy McConnell <remy.m@floversal.com>
2020-01-11 14:07:12 +0100

    javadocs post controllers

--- src/ui/controller/post/WritePostController.java
+++ src/ui/controller/post/WritePostController.java
@@ -7,17 +7,33 @@ import javafx.scene.control.TextArea;
 import ui.interfaces.LoginInterface;
 import ui.model.ApplicationUI;
 
+/**
+ * The type Write post controller.
+ */
 public class WritePostController implements LoginInterface {
     private PostFacade PF = PostFacade.getInstance(this);
     private SessionFacade SF = SessionFacade.getInstance(this);
 
+    /**
+     * The text area to write the post.
+     */
     @FXML
     TextArea content;
 
+    /**
+     * Go back to posts view.
+     *
+     * @throws Exception the exception
+     */
     public void backToPosts() throws Exception {
         ApplicationUI.postView(ApplicationUI.getStage());
     }
 
+    /**
+     * Publish the post.
+     *
+     * @throws Exception the exception
+     */
     public void publishPost() throws Exception {
         if (!content.getText().isEmpty()) {
             PF.writePost(SF.getUserLoggedIn().getUsername(), content.getText());
