ad43511bfa8c099e02dd839a5a4410409a6b6d38
Remy McConnell <remy.m@floversal.com>
2020-01-14 10:34:30 +0100

    fix avec les polices

--- src/resources/styles/ElementLabels.java
+++ src/resources/styles/ElementLabels.java
@@ -8,6 +8,7 @@ public class ElementLabels extends Label {
 
     public ElementLabels(String text){
         super(text);
-        setFont(new Font("Arial Rounded MT Bold", 15));
+//        setFont(new Font("Arial Rounded MT Bold", 15));
+        setFont(Font.loadFont(getClass().getResourceAsStream("/resources/fonts/ArialRoundedMTBold.ttf"), 15));
     }
 }
